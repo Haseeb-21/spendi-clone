e9e926479c0e35a8c3725fb517270c06a6598653
Haseeb-21 <71402178+Haseeb-21@users.noreply.github.com>
2021-05-14 22:41:27 -0700

    file

---
+++ Models/expense_manager.py
@@ -0,0 +1,201 @@
+from Models.expense import Expense
+# from expense import Expense
+import csv
+from datetime import datetime, timedelta
+
+
+class ExpenseManager:
+
+    def __init__(self):
+        self._expenses = {}
+
+    
+    def add_expense(self, expense):
+        """ Add a new expense to the Expense Manager
+
+        :param expense: A new expense from user
+        :type expense: Expense
+        :raises TypeError: Valid for class Expense only
+        """
+        if not isinstance(expense, Expense):
+            raise TypeError("Invalid expense type!")
+        self._expenses[expense.ID] = expense
+
+
+    def del_expense(self, expenseID):
+        """ Delete the expense from Expense Manager based on the expense ID"""
+        self._expenses.pop(expenseID)
+    
+
+    def upd_expense(self, expenseID, expense):
+        """ Update the expense from Expense Manager based on the expense ID"""
+        self._expenses[expenseID] = expense
+
+
+    def get_expenses(self):
+        """Return a list of expenses (in dict) that stored inside the Expense Maanger """
+
+        expenses_list = []
+        for (key, value) in self._expenses.items():
+            expenses_list.append(value)
+        return [ expense.to_dict() for expense in expenses_list]
+
+
+    def get_details(self, ID):
+        """
+        Returns attribute values of expense using its ID
+
+        :param budget
+        :type budget: int
+        """
+        return self._expenses.get(ID)
+        
+
+    def from_csv(self, csv_file):
+        """Get data from a csv file
+
+        :param csv_file: csv file to get from
+        :type csv_file, i.e. the filepath
+        """
+        with open(csv_file, "r") as f:
+            reader = csv.DictReader(f)
+            for item in reader:
+                expense = Expense.create_from_dict(item)
+                self.add_expense(expense)
+
+
+    def to_csv(self, csv_file):
+        """Append to a csv file"""
+        fields = Expense.get_serializable_field_names()
+        with open(csv_file, "a", newline="") as f:
+            writer = csv.DictWriter(f, fields)
+            # writer.writeheader()
+            for expense in self._expenses.values():
+                writer.writerow(expense.to_dict())
+
+
+    def override_to_csv(self, csv_file):
+        """Write to a csv file"""
+        fields = Expense.get_serializable_field_names()
+        with open(csv_file, "w", newline="") as f:
+            writer = csv.DictWriter(f, fields)
+            writer.writeheader()
+            for expense in self._expenses.values():
+                writer.writerow(expense.to_dict())
+
+
+    def read_largest_id(self, csv_file):
+        """Get largest id from csv_file
+
+        :param csv_file: csv file to get from
+        :type csv_file, i.e. the filepath
+        """
+        id = []
+        with open(csv_file, "r") as f:
+            reader = csv.DictReader(f)
+            for item in reader:
+                expense = Expense.create_from_dict(item)
+                id.append(expense.ID)
+            id.sort(reverse=True)
+        # Check if the csv have no record at all
+        if len(id) == 0:
+            largest_ID = 0
+        else:
+            largest_ID = id[0]
+        return largest_ID
+            
+
+    def by_month_expense(self):
+        """ Calculate the expenses by month (last 1 year)"""
+        
+        dict_month = {
+            "Jan": 0.0,
+            "Feb": 0.0,
+            "Mar": 0.0,
+            "Apr": 0.0,
+            "May": 0.0,
+            "Jun": 0.0,
+            "Jul": 0.0,
+            "Aug": 0.0,
+            "Sep": 0.0,
+            "Oct": 0.0,
+            "Nov": 0.0,
+            "Dec": 0.0
+            }
+        TODAY = datetime.today()
+
+        for (key, value) in self._expenses.items():
+            if TODAY - value.Date <= timedelta(365):
+                if value.Date.month == 1:
+                    dict_month["Jan"] = dict_month["Jan"] + float(value.Amount)
+                if value.Date.month == 2:
+                    dict_month["Feb"] = dict_month["Feb"] + float(value.Amount)
+                if value.Date.month == 3:
+                    dict_month["Mar"] = dict_month["Mar"] + float(value.Amount)
+                if value.Date.month == 4:
+                    dict_month["Apr"] = dict_month["Apr"] + float(value.Amount)    
+                if value.Date.month == 5:
+                    dict_month["May"] = dict_month["May"] + float(value.Amount)
+                if value.Date.month == 6:
+                    dict_month["Jun"] = dict_month["Jun"] + float(value.Amount)
+                if value.Date.month == 7:
+                    dict_month["Jul"] = dict_month["Jul"] + float(value.Amount)
+                if value.Date.month == 8:
+                    dict_month["Aug"] = dict_month["Aug"] + float(value.Amount)
+                if value.Date.month == 9:
+                    dict_month["Sep"] = dict_month["Sep"] + float(value.Amount)
+                if value.Date.month == 10:
+                    dict_month["Oct"] = dict_month["Oct"] + float(value.Amount)
+                if value.Date.month == 11:
+                    dict_month["Nov"] = dict_month["Nov"] + float(value.Amount)
+                if value.Date.month == 12:
+                    dict_month["Dec"] = dict_month["Dec"] + float(value.Amount)
+
+        return dict_month
+
+
+    def by_category(self):
+        """ Calculate the expenses by caetgory """
+
+        # 1st element is subtotal; 2nd element is percentage
+        dict_category = {
+            "School": [0.0, 0.0],
+            "Food": [0.0, 0.0],
+            "Health": [0.0, 0.0],
+            "Family": [0.0, 0.0]}
+        TODAY = datetime.today()
+        Total = 0
+
+        # Calculate the total amouunt
+        for (key, value) in self._expenses.items():
+            if TODAY - value.Date <= timedelta(365):
+                Total = Total + float(value.Amount)
+
+        for (key, value) in self._expenses.items():
+            if TODAY - value.Date <= timedelta(365):
+                if value.Category == "School":
+                    dict_category["School"][0] = dict_category["School"][0] + float(value.Amount)
+                    dict_category["School"][1] = format(dict_category["School"][0] / Total * 100, ".2f")
+                if value.Category == "Food":
+                    dict_category["Food"][0] = dict_category["Food"][0] + float(value.Amount)
+                    dict_category["Food"][1] = format(dict_category["Food"][0] / Total * 100, ".2f")
+                if value.Category == "Health":
+                    dict_category["Health"][0] = dict_category["Health"][0] + float(value.Amount)
+                    dict_category["Health"][1] = format(dict_category["Health"][0] / Total * 100, ".2f")
+                if value.Category == "Family":
+                    dict_category["Family"][0] = dict_category["Family"][0] + float(value.Amount)
+                    dict_category["Family"][1] = format(dict_category["Family"][0] / Total * 100, ".2f")
+            
+        return dict_category
+
+
+############################################
+if __name__ == "__main__":
+    EM = ExpenseManager()
+    EM.from_csv("expense.csv")
+
+    print(EM.get_expenses())    
+    print(EM.read_largest_id("expense.csv"))
+
+    # EM.del_expense(2)
+    # print(EM.get_expenses())    
---
+++ app.py
@@ -0,0 +1,248 @@
+from flask import Flask, request, render_template, url_for, redirect, flash
+import csv
+from Models.expense import Expense
+from Models.expense_manager import ExpenseManager
+
+
+app = Flask(__name__)
+app.secret_key = b'ooo_5#y2L"F4Q9858z\n\xec]/'
+
+# Database filename: csv file for expense and balance/budget
+expense_csv = "Models\expense.csv"
+balance_csv = "data.csv"
+
+
+def create_csv(balance, budget):
+        with open(balance_csv, 'w', newline = '') as f:
+            writer = csv.writer(f)
+            rows = [balance, budget]
+
+            fields = ["Balance", "Budget"]
+            
+            # Writes fields and rows to csv
+            writer.writerow(fields)
+            writer.writerow(rows)
+
+
+def add_to_csv(balance, budget):
+    """ Add balance and budget to the csv file
+
+    :param balance: balance that to be updated
+    :type balance: float
+    :param budget: budget that to be updated
+    :type budget: float
+    """
+    # Appends to data.csv instead of writing, which would replace existing entries
+    with open(balance_csv, 'a', newline = '') as f:
+        writer = csv.writer(f)
+        rows = [balance, budget]
+        writer.writerow(rows)
+
+
+def from_csv(csv_file):
+    """ Load the balance and budget from the csv file 
+    
+    :return: Latest balance and budget from the csv file
+    :rtype: dict
+    """
+    balanceBudget = {}
+    with open(csv_file, "r") as f:
+        reader = csv.DictReader(f)
+        for item in reader:
+            balanceBudget["balance"] = format(float(item["balance"]), ".2f")
+            balanceBudget["budget"] = format(float(item["budget"]), ".2f")
+    
+    return balanceBudget
+                
+
+def list_all_expenses():
+    """ List all expenses in the csv file
+
+    :return: All expenses records
+    :rtype: dict
+    """
+    EM = ExpenseManager()
+    EM.from_csv(expense_csv)
+    return {"expenses": EM.get_expenses()}
+    
+
+def display_expense_by_month():
+    """ Summarize all expesnses by month 
+
+    :return: All expenses subtotal in last 12 months
+    :rtype: dict
+    """
+    EM = ExpenseManager()
+    EM.from_csv(expense_csv)
+    return EM.by_month_expense()
+
+
+def display_expense_by_category():
+    """ Summarize all expenses by category
+
+    :return: All expenses subtotal by category in last 12 months, with percentage
+    :rtype: dict
+    """
+    EM = ExpenseManager()
+    EM.from_csv(expense_csv)
+    return EM.by_category()
+
+
+def delete_expense(ID):
+    """ Delete the expense record from the list
+
+    :return: None
+    :rtype: None
+    """
+    # Load the list of expenses from csv, then put it into EM to manipulate
+    # Finally put it back to the csv
+    EM = ExpenseManager()
+    EM.from_csv(expense_csv)
+    EM.del_expense(ID)
+    EM.override_to_csv(expense_csv)
+
+
+def update_expense(ID, category, amount, date):
+    EM = ExpenseManager()
+    EM.from_csv(expense_csv)
+
+    expense = Expense(ID, category, amount, date)
+    EM.upd_expense(ID, expense)
+    EM.override_to_csv(expense_csv)
+
+
+##################################################################
+### Routes from here ###
+
+@app.route('/')
+def index():  
+    return render_template(
+        "main.html", 
+        expenses=list_all_expenses(), 
+        balanceBudget=from_csv(balance_csv), 
+        byMonth=display_expense_by_month(),
+        byCategory=display_expense_by_category(),
+        )
+
+
+@app.route("/delete/<int:ID>")
+def delete(ID):
+    delete_expense(ID)
+    flash(f'Expense #{ID} has been deleted!')
+    return redirect(url_for("index"))
+
+
+@app.route("/update/<int:ID>")
+def edit(ID):
+    EM = ExpenseManager()
+    EM.from_csv(expense_csv)
+    expense = EM.get_details(ID)
+    
+    category = getattr(expense, "_Category")
+    amount = getattr(expense, "_Amount")
+    date = getattr(expense, "_Date")
+    category=category.lower()
+    
+    return render_template("update.html", ID=ID,category=category, amount=amount, date=date)
+
+
+@app.route("/update/<int:ID>", methods=['POST'])
+def update(ID):
+    try:
+        category = request.form['category']
+        amount = float(request.form['amount'])
+        date = request.form['date']
+    except ValueError:
+        flash("Please enter a valid number for expense amount")
+        return redirect(url_for("edit", ID=ID))
+    except KeyError:
+        return redirect(url_for("index"))
+
+    update_expense(ID, category, amount, date)
+    flash(f'Expense #{ID} has been updated!')
+    return redirect(url_for("index"))
+
+
+@app.route("/add",  methods=['POST'])
+def expense():
+    ## Expense ##
+
+    # Get input from html
+    try:
+        category = request.form['category']
+    except KeyError:
+        flash("Please select a category")
+        return redirect(url_for("index"))
+
+    try:
+        amount = float(request.form['amount'])
+    except ValueError:
+        flash("Please enter a valid number for expense amount")
+        return redirect(url_for("index"))
+
+    
+
+    if category != "":
+        # Store as a class Expense object
+        EM = ExpenseManager()
+        Next_ID = EM.read_largest_id(expense_csv) + 1 # Assign the next ID#
+        expense = Expense(Next_ID, category, amount)
+        
+        # Add the expense into the Expense Manager
+        EM.add_expense(expense)
+        
+        # Deduct expense amount from balance
+        bal_dict=from_csv(balance_csv)
+        bal_dict["balance"] = float(bal_dict["balance"]) - float(expense.Amount)
+        
+        # Save expense
+        EM.to_csv(expense_csv)
+
+        # Save balance, budget
+        add_to_csv(bal_dict["balance"], bal_dict["budget"])
+    elif len(category) == 0:
+        flash("Category can't be left empty")
+        return redirect(url_for("index"))
+
+    flash(f'Expense #{Next_ID} has been added!')
+    return redirect(url_for("index"))
+
+
+@app.route('/', methods=['POST'])
+def balanceBudget():
+    ## Balance ##
+    try:
+        balance = float(request.form['balance'])
+    except ValueError:
+        balance = request.form['balance']
+        if balance == "":
+            balance = from_csv(balance_csv)["balance"]
+        else:
+            flash("Please enter a valid number for balance amount")
+        balance = from_csv(balance_csv)["balance"]
+
+    
+    try:
+        budget = float(request.form['budget'])
+    except ValueError:
+        budget = request.form['budget']
+        if budget == "":
+            budget = from_csv(balance_csv)["budget"]
+        else:
+            flash("Please enter a valid number for budget amount")
+        budget = from_csv(balance_csv)["budget"]
+        
+
+    # Check if the input box is empty or non-integer value entered
+    try: 
+        with open(balance_csv, 'a') as f:
+            add_to_csv(balance, budget)
+    except FileNotFoundError:
+        create_csv(balance, budget)
+    
+    return redirect(url_for("index"))
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
